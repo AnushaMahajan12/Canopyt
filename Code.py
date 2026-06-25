fc7502edce09e3ab1d5c22fb7bb56bf76dbcb71f
AnushaMahajan12 <144406272+AnushaMahajan12@users.noreply.github.com>
2026-06-25 16:45:34 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -12,68 +12,52 @@ import gradio as gr
 
 csv = "https://raw.githubusercontent.com/AnushaMahajan12/Canopyt/refs/heads/main/ShadeData.csv"
 tree_data = pd.read_csv(csv)
-threshold_values = []
-not_threshold_values = []
-for score in tree_data['MasterScore']:
-    if score >= 0.5:
-        threshold_values.append(score)
-        not_threshold_values.append(0)
-    else:
-        not_threshold_values.append(score)
-        threshold_values.append(0)
-masscore_values = []
-for index, row in tree_data.iterrows():
-    traffic_index = row['TrafficProximityIndex']
-    pop_density = row['PopDensityIndex']
-    air_quality = row['AirQualityIndex']
-    low_income = row['LowIncomeIndex']
-    heat_imperv = row['HeatImpervIndex']
-    canopy_cover = row['CanopyCoverIndex']
-    new_master_score = (traffic_index + pop_density + air_quality + low_income + heat_imperv + canopy_cover)/6
-    masscore_values.append(new_master_score)
-tree_data['NewMasterScore'] = masscore_values
-thresholds = []
-for index, row in tree_data.iterrows():
-    traffic_index = row['TrafficProximityIndex']
-    pop_density = row['PopDensityIndex']
-    air_quality = row['AirQualityIndex']
-    low_income = row['LowIncomeIndex']
-    heat_imperv = row['HeatImpervIndex']
-    new_master_score = row['NewMasterScore']
-    canopy_differences = 1.5 - traffic_index - pop_density - air_quality - low_income - heat_imperv
-    thresholds.append(canopy_differences)
-tree_data['CanopyCoverDifference'] = thresholds
 
+#Creates new column for what lack of canopy cover has to be for MasterScore = 0.25
+canCovDif = []
+for index, row in tree_data.iterrows(): #1.5 = 0.25*6 = average MasterScore excluding the Canopy Cover --> subtracting all the indices = difference of canopy needed to make city reach threshold sustainability
+  canopyDifferences = 1.5 - (row['TrafficProximityIndex'] + row['PopDensityIndex'] + row['AirQualityIndex'] + row['LowIncomeIndex'] + row['HeatImpervIndex'])
+  canCovDif.append(canopyDifferences)
+tree_data['CanopyCoverDifference'] = canCovDif 
+
+#Updates csv to add column of difference
 tree_data.to_csv('updated_tree_data.csv', index=False)
+#files.download('updated_tree_data.csv')
 model_data = pd.read_csv('updated_tree_data.csv')
 
-model_data = model_data.dropna(subset=['TrafficProximityIndex', 'AirQualityIndex', 'LowIncomeIndex', 'CanopyCoverDifference'])
+model_data = model_data.dropna(subset=['TrafficProximityIndex', 'AirQualityIndex', 'LowIncomeIndex', 'CanopyCoverDifference']) #chosen indices drop any values that don't exist
 model_data.to_csv('final_tree_data.csv', index=False)
+#files.download('final_tree_data.csv')
 
-X = model_data[['TrafficProximityIndex', 'AirQualityIndex', 'LowIncomeIndex']]
-y = model_data['CanopyCoverDifference']
+X = model_data[['TrafficProximityIndex', 'AirQualityIndex', 'LowIncomeIndex']] #inputs
+y = model_data['CanopyCoverDifference'] #output
+
+#need a constant --> linear regression model = mx + !b!
 X = sm.add_constant(X)
-model = sm.OLS(y, X).fit()
-print(model.summary())
+model = sm.OLS(y, X).fit() #fits model 
 
-def predict(TrafficProximityIndex, AirQualityIndex, LowIncomeIndex):
+#print(model.summary())
+#directly related to Canopy Cover Difference --> very low coef (for one unit of Index --> drops by that much for CCD)
+#Durbin Watson --> explains how data is clustered (one area with low score = next to another with low score)
+
+def predictCan(TrafficProximityIndex, AirQualityIndex, LowIncomeIndex): #define predict as input 3 indices
   input = pd.DataFrame([{
       'TrafficProximityIndex': float(TrafficProximityIndex),
       'AirQualityIndex': float(AirQualityIndex),
       'LowIncomeIndex': float(LowIncomeIndex)
       }])
-  input.insert(0, 'const', 1.0)
-  canopy_cover = model.predict(input)[0]
-  return f'Predicted Canopy Cover Difference: {canopy_cover: .2f}'
+  input.insert(0, 'constant', 1.0) #acts as constant (need to add other dimension due to linear constant)
+  canopyCover = model.predict(input)[0] #model predict cover w/ input
+  return f'Predicted Canopy Cover Difference: {canopyCover: .2f}'
 
-interface = gr.Interface(
-    fn = predict,
+interface = gr.Interface( #creates interface
+    fn = predictCan, #predict the canopy cover dif w/ sliders for inputs all in order
     inputs = [
         gr.Slider(minimum = 0.00, maximum = 1.00, value = 0.50, label = "Traffic Proximity Index"),
         gr.Slider(minimum = 0.00, maximum = 1.00, value = 0.50, label = "Air Quality Index"),
         gr.Slider(minimum = 0.00, maximum = 1.00, value = 0.50, label = "Low Income Index")
     ],
-    outputs = gr.Textbox(label = "Model Prediction"),
+    outputs = gr.Textbox(label = "Model Prediction"), #labels on interface
     title = "Canopy Need Predictor",
     description = "Adjust the different indices to fit the city to determine a prediction for how much canopy cover is needed to make the city more sustainable."
 )
